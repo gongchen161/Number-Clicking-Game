e3505752c77c80a309f46c55c6fa011e9aeae0cc
Gong Chen <gong.chen161@gmail.com>
2018-04-01 00:50:43 -0400

    Enable random numbers

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
 
 var btn = [];
 var len = 16;
+var play;
+
+var flag = new Array(100, false);
+var ans = [];
 
 function init() {
 	for (var i = 0; i<len; i++) {
 		btn[i] = document.getElementById("btn"+i);
-		btn[i].innerHTML = "XX";
 
-		btn[i].onclick = function() {
-			this.innerHTML = "CC";
+		var cur = Math.floor(Math.random() * 100);
+		while (flag[cur]) {
+			cur = Math.floor(Math.random() * 100);
 		}
+		flag[cur] = true;
+		btn[i].innerHTML = cur;
 	}
 }
 
 
 window.onload = function() {
 	init();
+	play = document.getElementById("play");
+}
+
+play.onclick = function() {
+		init();
 }
