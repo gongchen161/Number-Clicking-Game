5537e4ad89a9a2fa38ff51bdd21ab87086928551
Gong Chen <gong.chen161@gmail.com>
2018-04-01 00:03:22 -0400

    Adding basic script

---
+++ script.js
@@ -0,0 +1,18 @@
+
+var btn0;
+var btn1;
+var btn2;
+var btn3;
+
+window.onload = function() {
+	btn0 = document.getElementById("btn0");
+	btn1 = document.getElementById("btn1");
+	btn2 = document.getElementById("btn2");
+	btn3 = document.getElementById("btn3");
+
+
+	btn0.innerHTML = 1;
+	btn1.innerHTML = 10;
+	btn2.innerHTML = 100;
+	btn3.innerHTML = 1000;
+}
