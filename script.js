3754f7a4fe7c6b1793a4d9d92bfcf5542935070e
Gong Chen <gong.chen161@gmail.com>
2018-04-01 01:09:35 -0400

    First version

--- script.js
+++ script.js
@@ -5,8 +5,14 @@ var play;
 
 var flag = new Array(100, false);
 var ans = [];
+var curTerm = 0;
+var done = false;
 
 function init() {
+	curTerm = 0;
+	done = false;
+	ans = []
+	flag = new Array(100, false);
 	for (var i = 0; i<len; i++) {
 		btn[i] = document.getElementById("btn"+i);
 
@@ -16,13 +22,23 @@ function init() {
 		}
 		flag[cur] = true;
 		btn[i].innerHTML = cur;
+		ans.push(cur);
+
+		btn[i].onclick = function() {
+			if (this.innerHTML == ans[curTerm]) {
+				this.innerHTML = "X";
+				curTerm++;
+
+				if (curTerm == 16) {
+					alert("You Won");
+				}
+			} else {
+				alert(ans[curTerm]);
+			}
+		}
 	}
-}
 
-
-window.onload = function() {
-	init();
-	play = document.getElementById("play");
+	ans.sort(function(a, b){return a - b});
 }
 
 play.onclick = function() {
